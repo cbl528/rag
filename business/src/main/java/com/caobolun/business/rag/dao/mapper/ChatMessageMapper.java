7f25f701471d8c4e6b101c33c0e377d3b74c7d9b
cbl <cao_bolun@qq.com>
2026-07-12 08:29:36 +0800

    新增会话相关实体类和接口

---
+++ business/src/main/java/com/caobolun/business/rag/dao/mapper/ChatMessageMapper.java
@@ -0,0 +1,9 @@
+package com.caobolun.business.rag.dao.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.caobolun.business.rag.dao.entity.ChatMessageEntity;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface ChatMessageMapper extends BaseMapper<ChatMessageEntity> {
+}
---
+++ business/src/main/java/com/caobolun/business/rag/dao/mapper/ChatSessionMapper.java
@@ -0,0 +1,9 @@
+package com.caobolun.business.rag.dao.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.caobolun.business.rag.dao.entity.ChatSessionEntity;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface ChatSessionMapper extends BaseMapper<ChatSessionEntity> {
+}
